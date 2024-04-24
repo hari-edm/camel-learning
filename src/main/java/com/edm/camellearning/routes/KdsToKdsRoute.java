81bda236e64a7bcf5ba5a4b9385a7844d053b6eb
Hari <krishari231@gmail.com>
2024-04-24 11:27:56 +0530

    Cleanup

--- src/main/java/com/edm/camellearning/routes/KdsToKdsRoute.java
+++ src/main/java/com/edm/camellearning/routes/KdsToKdsRoute.java
@@ -5,29 +5,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-
-
 @Component
 public class KdsToKdsRoute extends RouteBuilder {
-    Logger logger = LoggerFactory.getLogger(KdsToKdsRoute.class);
-    @Override
-    public void configure() throws Exception {
+  Logger logger = LoggerFactory.getLogger(KdsToKdsRoute.class);
 
-     //   from("aws2-kinesis://my-test-input-kds?useDefaultCredentialsProvider=true")
-       from("myscheme://my-test-input-kds")
-              //  .to("log://abcd");
-                .to("myscheme://abcd");
-               // .to("aws2-kinesis://my-test-producer-kds?useDefaultCredentialsProvider=true");
-                /*.to("seda:process");*/
+  @Override
+  public void configure() throws Exception {
 
-       /* from("seda:process?concurrentConsumers=5") //.to("log://abcd");
-                .process(new Processor() {
+    //   from("aws2-kinesis://my-test-input-kds?useDefaultCredentialsProvider=true")
+    from("myscheme://my-test-input-kds")
+        //  .to("log://abcd");
+        .to("myscheme://abcd");
+    // .to("aws2-kinesis://my-test-producer-kds?useDefaultCredentialsProvider=true");
 
-                    public void process(Exchange exchange) throws Exception {
-                        logger.info(exchange.getMessage().getBody().toString());
-                        System.out.println(Thread.currentThread().getName() + " " +exchange.getMessage().getBody());
-                    }
-                });*/
-
-    }
+  }
 }
